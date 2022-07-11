daca08f72eb4a2aad472cae4a76e48482254951b
Ilyz <gorbacevaili891@gmail.com>
2022-07-11 22:06:57 +0300

    added method-query for checking exist username

--- src/main/java/softarex/gorbachev/springbootrestclassroom/repository/UserRepository.java
+++ src/main/java/softarex/gorbachev/springbootrestclassroom/repository/UserRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import softarex.gorbachev.springbootrestclassroom.model.User;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository  extends CrudRepository<User, UUID> {
+public interface UserRepository extends CrudRepository<User, UUID> {
 
+    boolean existsByName(String name);
 }
