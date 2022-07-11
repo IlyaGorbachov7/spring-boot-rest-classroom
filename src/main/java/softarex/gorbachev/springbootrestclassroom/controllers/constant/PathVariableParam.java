f98678ddce8998f6d3d722bc050686a904830f71
Ilyz <gorbacevaili891@gmail.com>
2022-07-11 17:03:09 +0300

    created file contains CONSTANTS for UserController.java

---
+++ src/main/java/softarex/gorbachev/springbootrestclassroom/controllers/constant/PathVariableParam.java
@@ -0,0 +1,5 @@
+package softarex.gorbachev.springbootrestclassroom.controllers.constant;
+
+public interface PathVariableParam {
+    String USER_ID = "userId";
+}
---
+++ src/main/java/softarex/gorbachev/springbootrestclassroom/controllers/constant/UrlPath.java
@@ -0,0 +1,8 @@
+package softarex.gorbachev.springbootrestclassroom.controllers.constant;
+
+public interface UrlPath {
+    String CROSS_ORIGIN = "http://localhost:3000";
+    String USER_PATH = "/users";
+    String USER_ID_PATH = "/{userId}";
+
+}
