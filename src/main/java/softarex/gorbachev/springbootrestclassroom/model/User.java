d47dd4b891a02c747a7cbcee9aeae69961f3ef43
Ilyz <gorbacevaili891@gmail.com>
2022-07-11 22:05:06 +0300

    added annotation @Indexed for query to database

--- src/main/java/softarex/gorbachev/springbootrestclassroom/model/User.java
+++ src/main/java/softarex/gorbachev/springbootrestclassroom/model/User.java
@@ -1,9 +1,12 @@
 package softarex.gorbachev.springbootrestclassroom.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -12,16 +15,19 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @RedisHash("User")
 public class User implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 1603714798906422731L;
+
+    @Id
     private UUID id;
+    @Indexed
     private String name;
     private boolean isHand;
 
-    public void update(User user){
+    public void update(User user) {
         name = user.name;
         isHand = user.isHand;
     }
 }
+
