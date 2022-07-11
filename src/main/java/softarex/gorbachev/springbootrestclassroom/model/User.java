2e269302e0dfddf0c33a8940456725d32f2b5399
Ilyz <gorbacevaili891@gmail.com>
2022-07-11 17:01:45 +0300

    id => is UUID. Added method to update current User.java

--- src/main/java/softarex/gorbachev/springbootrestclassroom/model/User.java
+++ src/main/java/softarex/gorbachev/springbootrestclassroom/model/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.UUID;
 
@@ -13,8 +14,14 @@ import java.util.UUID;
 @NoArgsConstructor
 @RedisHash("User")
 public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1603714798906422731L;
     private UUID id;
     private String name;
     private boolean isHand;
 
+    public void update(User user){
+        name = user.name;
+        isHand = user.isHand;
+    }
 }
