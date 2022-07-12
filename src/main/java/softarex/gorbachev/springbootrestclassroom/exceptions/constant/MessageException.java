6cf0e591151808bb5dbb386d159d8191c5cb5656
Ilyz <gorbacevaili891@gmail.com>
2022-07-12 12:54:05 +0300

    files contains message exceptions

---
+++ src/main/java/softarex/gorbachev/springbootrestclassroom/exceptions/constant/MessageException.java
@@ -0,0 +1,13 @@
+package softarex.gorbachev.springbootrestclassroom.exceptions.constant;
+
+public interface MessageException {
+    String ID_IS_NULL = "id is null";
+
+    String REQUEST_BODY_IS_NULL = "request body is null";
+
+    String USER_NO_FOUND_BY_ID = "no found user by id";
+
+    String USERNAME_IS_EXIST = "username is exist";
+
+    String USER_ID_IS_EXIST = "user id is exist";
+}
