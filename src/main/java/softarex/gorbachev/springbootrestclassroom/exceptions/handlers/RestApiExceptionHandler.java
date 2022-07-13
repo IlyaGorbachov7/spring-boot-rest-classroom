426f8eeffa0ac343f58944381bff43bbdb92935f
Ilyz <gorbacevaili891@gmail.com>
2022-07-14 00:30:31 +0300

    added handlers exceptions

--- src/main/java/softarex/gorbachev/springbootrestclassroom/exceptions/handlers/RestApiExceptionHandler.java
+++ src/main/java/softarex/gorbachev/springbootrestclassroom/exceptions/handlers/RestApiExceptionHandler.java
@@ -2,15 +2,53 @@ package softarex.gorbachev.springbootrestclassroom.exceptions.handlers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import softarex.gorbachev.springbootrestclassroom.exceptions.UserServiceException;
 
+import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * THis class represent space for to handle certain
+ * exception within entire application
+ *
+ * @author Gorabachev I. D.
+ * @version 14.07.2022
+ */
 @ControllerAdvice
 public class RestApiExceptionHandler {
 
     @ExceptionHandler(value = {UserServiceException.class})
-    public ResponseEntity<String> handleException(Exception ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleException(Exception ex) {
+        Map<String, String> map = new HashMap<>();
+        map.put("error", ex.getMessage());
+
+        return map;
     }
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> map = new HashMap<>();
+        ex.getFieldErrors().forEach(fieldError -> map.put(fieldError.getField(),
+                fieldError.getDefaultMessage()));
+
+        return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    Map<String, String> handleConstraintViolationException(ConstraintViolationException e) {
+        Map<String, String> map = new HashMap<>();
+        e.getConstraintViolations().forEach(violation -> map.put(violation.getPropertyPath().toString(),
+                violation.getMessage()));
+
+        return map;
+    }
+
+
 }
