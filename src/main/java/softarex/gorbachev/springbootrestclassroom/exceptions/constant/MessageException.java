1610841734c908d269227915048a911584673023
Ilyz <gorbacevaili891@gmail.com>
2022-07-13 13:44:38 +0300

    added message

--- src/main/java/softarex/gorbachev/springbootrestclassroom/exceptions/constant/MessageException.java
+++ src/main/java/softarex/gorbachev/springbootrestclassroom/exceptions/constant/MessageException.java
@@ -3,6 +3,8 @@ package softarex.gorbachev.springbootrestclassroom.exceptions.constant;
 public interface MessageException {
     String ID_IS_NULL = "id is null";
 
+    String NAME_IS_NULL = "name is null";
+
     String REQUEST_BODY_IS_NULL = "request body is null";
 
     String USER_NO_FOUND_BY_ID = "no found user by id";
