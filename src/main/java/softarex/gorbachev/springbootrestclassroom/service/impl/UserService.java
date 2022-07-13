8bf0b43b0a5067b15b74616b7ffa95ac6b6047cd
Ilyz <gorbacevaili891@gmail.com>
2022-07-13 10:26:24 +0300

    User => UserDTO

--- src/main/java/softarex/gorbachev/springbootrestclassroom/service/impl/UserService.java
+++ src/main/java/softarex/gorbachev/springbootrestclassroom/service/impl/UserService.java
@@ -1,12 +1,12 @@
 package softarex.gorbachev.springbootrestclassroom.service.impl;
 
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 import softarex.gorbachev.springbootrestclassroom.exceptions.UserServiceException;
 import softarex.gorbachev.springbootrestclassroom.exceptions.constant.MessageException;
 import softarex.gorbachev.springbootrestclassroom.model.User;
+import softarex.gorbachev.springbootrestclassroom.model.dto.UserDTO;
+import softarex.gorbachev.springbootrestclassroom.model.mappers.UserMapper;
 import softarex.gorbachev.springbootrestclassroom.repository.UserRepository;
 import softarex.gorbachev.springbootrestclassroom.service.ServiceTemplate;
 
@@ -15,65 +15,79 @@ import java.util.*;
 
 @Service
 @AllArgsConstructor
-public class UserService implements ServiceTemplate<User, UUID> {
+public class UserService implements ServiceTemplate<UserDTO, UUID> {
 
     private final UserRepository repository;
 
-    @Override
-    public List<User> getAll() {
-        List<User> users = new ArrayList<>();
-        repository.findAll().forEach(users::add);
+    private final UserMapper mapper;
 
-        return users;
+    @Override
+    public List<UserDTO> getAll() {
+        return mapper.mapList(repository.findAll());
     }
 
     @Override
-    public User getById(UUID uuid) {
-        if (Objects.isNull(uuid)) throw new UserServiceException(MessageException.ID_IS_NULL);
+    public UserDTO getById(UUID uuid) {
+        if (Objects.isNull(uuid)) {
+            throw new UserServiceException(MessageException.ID_IS_NULL);
+        }
 
         Optional<User> row = repository.findById(uuid);
-        if (row.isEmpty()) throw new UserServiceException(MessageException.USER_NO_FOUND_BY_ID);
+        if (!row.isPresent()) {
+            throw new UserServiceException(MessageException.USER_NO_FOUND_BY_ID);
+        }
 
         User user = row.get();
-        return user;
+
+        return mapper.map(user);
     }
 
     @Override
-    public User create(User entity) {
-        if (Objects.isNull(entity))
+    public UserDTO create(UserDTO entity) {
+        if (Objects.isNull(entity)) {
             throw new UserServiceException(MessageException.REQUEST_BODY_IS_NULL);
+        }
 
-        if (!Objects.isNull(entity.getId()) && repository.existsById(entity.getId()))
+        if (!Objects.isNull(entity.getId()) && repository.existsById(entity.getId())) {
             throw new UserServiceException(MessageException.USER_ID_IS_EXIST);
+        }
 
-        if (Objects.isNull(entity.getName()) || entity.getName().isBlank()
-            || repository.existsByName(entity.getName()))
+        if (Objects.isNull(entity.getName()) || entity.getName().isEmpty()
+            || repository.existsByName(entity.getName())) {
             throw new UserServiceException(MessageException.USERNAME_IS_EXIST);
+        }
 
+        User user = mapper.map(entity); //  if isHand == true, then before conversation isHand == false
+        User saveUser = repository.save(user);
 
-        return repository.save(entity);
+        return mapper.map(saveUser);
     }
 
     @Override
-    public void update(User rscEntity) {
+    public UserDTO update(UserDTO rscEntity) {
         if (Objects.isNull(rscEntity) || Objects.isNull(rscEntity.getId()))
             throw new UserServiceException(MessageException.REQUEST_BODY_IS_NULL
                                            + " or " + MessageException.ID_IS_NULL);
 
-        if (repository.existsByName(rscEntity.getName()))
-            throw new UserServiceException(MessageException.USERNAME_IS_EXIST);
-
         Optional<User> row = repository.findById(rscEntity.getId());
-        if (row.isEmpty()) throw new UserServiceException(MessageException.USER_NO_FOUND_BY_ID);
+        if (!row.isPresent()) {
+            throw new UserServiceException(MessageException.USER_NO_FOUND_BY_ID);
+        }
 
         User existUser = row.get();
-        existUser.update(rscEntity);
-        repository.save(existUser);
+
+        User userFromDTO = mapper.map(rscEntity);
+        existUser.update(userFromDTO);
+        User saveUser = repository.save(existUser);
+
+        return mapper.map(saveUser);
     }
 
     @Override
     public void deleteById(UUID uuid) {
-        if (Objects.isNull(uuid)) throw new UserServiceException(MessageException.ID_IS_NULL);
+        if (Objects.isNull(uuid)) {
+            throw new UserServiceException(MessageException.ID_IS_NULL);
+        }
 
         repository.deleteById(uuid);
     }
