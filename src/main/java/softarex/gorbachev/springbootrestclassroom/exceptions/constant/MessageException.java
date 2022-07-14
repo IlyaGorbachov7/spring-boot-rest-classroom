22bc670563b90dc1e32b6b37e874eb98712c9218
Ilyz <gorbacevaili891@gmail.com>
2022-07-14 12:05:14 +0300

    removed some msg

--- src/main/java/softarex/gorbachev/springbootrestclassroom/exceptions/constant/MessageException.java
+++ src/main/java/softarex/gorbachev/springbootrestclassroom/exceptions/constant/MessageException.java
@@ -1,15 +1,18 @@
 package softarex.gorbachev.springbootrestclassroom.exceptions.constant;
 
+/**
+ * This interface contains definitions of exception message constants
+ *
+ * @author Gorabachev I. D.
+ * @version 13.07.2022
+ */
 public interface MessageException {
-    String ID_IS_NULL = "id is null";
-
-    String NAME_IS_NULL = "name is null";
-
-    String REQUEST_BODY_IS_NULL = "request body is null";
+    String USER_ID_IS_NULL = "user id is null";
 
     String USER_NO_FOUND_BY_ID = "no found user by id";
 
+    String USER_ID_IS_EXIST = "user id is exist";
+
     String USERNAME_IS_EXIST = "username is exist";
 
-    String USER_ID_IS_EXIST = "user id is exist";
 }
